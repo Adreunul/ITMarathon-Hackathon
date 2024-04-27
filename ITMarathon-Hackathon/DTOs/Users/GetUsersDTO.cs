a8ada9eb397fcde3508dc4d724dc922f6d59c20b
Adrian Danila <adrian-danila@365.univ-ovidius.ro>
2024-04-27 21:29:14 +0300

    push

---
+++ ITMarathon-Hackathon/DTOs/Users/GetUsersDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ITMarathon_Hackathon.DTOs.Users
+{
+    public class GetUsersDTO
+    {
+        public string username {  get; set; }
+        public string firstname {  get; set; }
+        public string lastname { get; set; }
+        public int isLogged { get; set; }
+        public float sold { get; set; }
+        public float soldInCoins { get; set; }
+    }
+}
---
+++ ITMarathon-Hackathon/Repositories/Users/GetUsersRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using ITMarathon_Hackathon.DTOs.Users;
+using ITMarathon_Hackathon.Interfaces;
+using System.Data;
+
+namespace ITMarathon_Hackathon.Repositories.Users
+{
+    public class GetUsersRepository
+    {
+        private readonly IDbConnectionFactory _connectionFactory;
+        public GetUsersRepository(IDbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+        public async Task<IEnumerable<GetUsersDTO>> GetUsersAsyncRepo()
+        {
+            using (var connection = _connectionFactory.ConnectToDataBase())
+            {
+                var result = await connection.QueryAsync<GetUsersDTO>("GetUsers", commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
